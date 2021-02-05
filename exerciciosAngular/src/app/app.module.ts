1ccb0da7a85afd48fd6655c64c3299b0c4a45c8f
Joao <joaopedrobarsaglini@gmail.com>
2021-02-04 21:28:45 -0300

    creating exercise component

--- exerciciosAngular/src/app/app.module.ts
+++ exerciciosAngular/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExercisesComponent } from './exercises/exercises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExercisesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ exerciciosAngular/src/app/exercises/exercises.component.ts
@@ -0,0 +1,27 @@
+import { OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercises',
+  templateUrl: './exercises.component.html',
+  styleUrls: ['./exercises.component.scss']
+})
+export class ExercisesComponent implements OnInit, OnDestroy {
+
+  public name: string = 'João P'
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.showMessage("")
+  }
+  ngOnDestroy(): void {
+    
+  }
+
+  public showMessage(name: string){
+    alert("Hello "+ name)
+
+  }
+
+}
